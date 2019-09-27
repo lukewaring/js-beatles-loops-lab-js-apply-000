8d503bd0cba674176f344ff9f249d36f2475e93c
Luke Waring <lukewaring@gmail.com>
2019-09-27 15:53:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  
+  for (let i = 0; i < musicians.length; i += 1) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return array;
+}
+
+// function johnLennonFacts(facts) {
+//   let array = [];
+  
+//   for (let i = 0; i < facts.length; i += 1) {
+//     array.push(`${facts[i]}!!!`);
+//   }
+  
+//   return array;
+// }
+
+function johnLennonFacts(facts) {
+  let array = [];
+  let i = 0;
+  
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i += 1;
+  }
+  
+  return array;
+}
+
+function iLoveTheBeatles(i) {
+  let array = [];
+  
+  do {
+    array.push('I love the Beatles!');
+    i += 1;
+  } while (i < 15);
+  
+  return array;
+}
